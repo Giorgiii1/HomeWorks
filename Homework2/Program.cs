1ef33befd6090eae45f4414defe212fcff69000b
giorgigongladze <93379003+Giorgiii1@users.noreply.github.com>
2026-05-25 16:55:52 +0400

    homework2 (#3)
    Co-authored-by: giorgi gongladze <ggongladze@gmail.com>

---
+++ Homework2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.BackgroundColor = ConsoleColor.Blue;
+Console.Clear();
+
+Console.WriteLine("Giorgi Gongladze");
+
+Console.Write("Please enter your input: ");
+
+string UserInput = Console.ReadLine();
+Console.WriteLine(UserInput);
